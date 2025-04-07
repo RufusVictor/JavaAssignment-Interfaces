21eb3bc0aa26d11b5e83d1580601d7cac7714564
Rufus Victor <139438518+RufusVictor@users.noreply.github.com>
2025-04-07 07:33:56 +0530

    Create ExperimentalRemoteControlCar.java

---
+++ ExperimentalRemoteControlCar.java
@@ -0,0 +1,10 @@
+public class ExperimentalRemoteControlCar implements RemoteControlCar {
+    private static final int SPEED = 20;
+    private int distance;
+    public void drive() {
+        distance += SPEED;
+    }
+    public int getDistanceTravelled() {
+        return distance;
+    }
+}
